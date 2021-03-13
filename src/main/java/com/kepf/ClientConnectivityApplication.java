9b002e27c0d5380027ce1ac80d69e78355d3d73b
achifred <achifred123@gmail.com>
2021-03-13 22:36:27 +0000

    initial commit

---
+++ src/main/java/com/kepf/ClientConnectivityApplication.java
@@ -0,0 +1,13 @@
+package com.kepf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientConnectivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientConnectivityApplication.class, args);
+	}
+
+}
